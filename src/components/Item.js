525af19cdc4d30f3a738c1edeafd747cdc8fdd1f
Becca Jonas <beccajonas3@gmail.com>
2023-11-21 15:04:19 -0500

    solutions, passing test with original code for instructor breakdown

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,6 +1,19 @@
 import React from "react";
 import {useState} from "react";
 
+// ! Lab code 
+// function Item({ name, category }) {
+//   return (
+//     <li className="">
+//       <span>{name}</span>
+//       <span className="category">{category}</span>
+//       <button className="add">Add to Cart</button>
+//     </li>
+//   );
+// }
+
+// export default Item;
+
 function Item({ name, category }) {
   let [isAdded, setIsAdded] = useState(false)
   function handleAdd() {
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -3,6 +3,30 @@ import Item from "./Item";
 import {useState} from "react";
 
 
+// ! Lab code 
+// function ShoppingList({ items }) {
+//   return (
+//     <div className="ShoppingList">
+//       <div className="Filter">
+//         <select name="filter">
+//           <option value="All">Filter by category</option>
+//           <option value="Produce">Produce</option>
+//           <option value="Dairy">Dairy</option>
+//           <option value="Dessert">Dessert</option>
+//         </select>
+//       </div>
+//       <ul className="Items">
+//         {items.map((item) => (
+//           <Item key={item.id} name={item.name} category={item.category} />
+//         ))}
+//       </ul>
+//     </div>
+//   );
+// }
+
+// export default ShoppingList;
+
+
 function ShoppingList({ items }) {
 
   const [selectedCategory, setSelectedCategory] = useState("All")
