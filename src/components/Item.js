dd2df85cea880c3db87069a8c7607c0a7a6e3729
Becca Jonas <beccajonas3@gmail.com>
2023-11-21 13:07:04 -0500

    solution

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import {useState} from "react";
 
 function Item({ name, category }) {
+  let [isAdded, setIsAdded] = useState(false)
+  function handleAdd() {
+    setIsAdded(!isAdded)
+  }
+
   return (
-    <li className="">
+    <li className={isAdded? "in-cart" : "" }>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleAdd} 
+      className= {isAdded? "remove": "add"}>{isAdded ? "Remove from Cart" : "Add to Cart"}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,35 @@
 import React from "react";
 import Item from "./Item";
+import {useState} from "react";
+
 
 function ShoppingList({ items }) {
+
+  const [selectedCategory, setSelectedCategory] = useState("All")
+
+  function handleFilter(e) {
+    console.log(e.target.value);
+    setSelectedCategory(e.target.value)
+  }
+
+  const itemsToDisplay = items.filter(item => {
+    if (selectedCategory === 'All') {
+      return true
+    } else {
+      return item.category === selectedCategory
+    }
+  })
+  
+  console.log(itemsToDisplay);
+  const filteredItems = itemsToDisplay.map(item => (
+    <Item key={item.id} name={item.name} category={item.category} />
+  ))
+
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select name="filter" onChange={handleFilter}>
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +37,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+        {filteredItems}
       </ul>
     </div>
   );
